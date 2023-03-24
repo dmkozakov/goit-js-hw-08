eab35f797797e45417a0a7921692a0a58b1888a0
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-03-24 14:46:48 +0200

    Add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,20 @@
 import VimeoPlayer from "@vimeo/player";
 var throttle = require("lodash.throttle");
 
+const STORAGE_KEY = "videoplayer-current-time";
+
 const refs = {
   iframe: document.querySelector("iframe"),
 };
 
 const player = new VimeoPlayer(refs.iframe);
-const savedTime = localStorage.getItem("videoplayer-current-time");
+const savedTime = localStorage.getItem(STORAGE_KEY);
 setTime(savedTime);
 
 player.on("timeupdate", throttle(saveCurrentTime, 1000));
 
 function saveCurrentTime(data) {
-  localStorage.setItem("videoplayer-current-time", data.seconds);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
 }
 
 function setTime(savedTime) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+var throttle = require("lodash.throttle");
 
+const STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+  feedbackForm: document.querySelector(".feedback-form"),
+  emailInput: document.querySelector('[name="email"]'),
+  messageInput: document.querySelector('[name="message"]'),
+};
+
+refs.feedbackForm.addEventListener("input", throttle(onFormInput, 500));
+refs.feedbackForm.addEventListener("submit", onSubmitBtn);
+
+const formData = {
+  email: "",
+  message: "",
+};
+saveFormData();
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitBtn(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.clear();
+}
+
+function saveFormData() {
+  try {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+      const parsedData = JSON.parse(savedData);
+
+      refs.emailInput.value = parsedData.email;
+      refs.messageInput.value = parsedData.message;
+    }
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
